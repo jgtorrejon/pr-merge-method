beebe971f2035836899b7945c07d20a8b33152a5
Torrejon Mendez Joel Gabriel <joel.torrejon.mendez@gmail.com>
2022-04-28 12:38:16 -0400

    hotfix: some fix (#11)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const App = () => {
   const constructor = title => {
-    return `<title>The title of master is ${title}<title/>`
+    return `<title>The title of hotfix is ${title}<title/>`
   }
 }
 
